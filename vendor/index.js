9e850ad4dbabdf7cf67ed2525ba6466505a28728
Rio Carpio <ryocarpio16@gmail.com>
2022-08-11 22:39:18 +0800

    Version 2

--- vendor/index.js
+++ vendor/index.js
@@ -1,193 +1,255 @@
 $(function () {
-	$('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
 $(document).ready(function () {
-	$("#aboutbtn").click(function () {
+  $("#aboutbtn").click(function () {
+    $(".toggle").css("border", "5px solid #000");
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+    return false;
+  });
+  $("#skillsbtn").click(function () {
+    $(".toggle").css("border", "5px solid #fff");
+    $("html, body").animate(
+      {
+        scrollTop: $("#skills").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+  $("#educationbtn").click(function () {
+    $(".toggle").css("border", "5px solid #fff");
+    $("html, body").animate(
+      {
+        scrollTop: $("#education").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+  $("#projectbtn").click(function () {
+    $(".toggle").css("border", "5px solid #fff");
+    $("html, body").animate(
+      {
+        scrollTop: $("#projects").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
 
-		$('.toggle').css('border', '5px solid #000')
-		$("html, body").animate({
-			scrollTop: 0
-		}, 1000);
-		return false;
-	});
-	$("#skillsbtn").click(function () {
-		$('.toggle').css('border', '5px solid #fff')
-		$("html, body").animate({
-			scrollTop: $("#skills").offset().top
-		}, 1000);
-		return false;
-	});
-	$("#educationbtn").click(function () {
-		$('.toggle').css('border', '5px solid #fff')
-		$("html, body").animate({
-			scrollTop: $("#education").offset().top
-		}, 1000);
-		return false;
-	});
-	$("#projectbtn").click(function () {
-		$('.toggle').css('border', '5px solid #fff')
-		$("html, body").animate({
-			scrollTop: $("#projects").offset().top
-		}, 1000);
-		return false;
-	});
+  $("#workbtn").click(function () {
+    $(".toggle").css("border", "5px solid #fff");
+    $("html, body").animate(
+      {
+        scrollTop: $("#workexp").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
 
+  $("#certbtn").click(function () {
+    $(".toggle").css("border", "5px solid #fff");
+    $("html, body").animate(
+      {
+        scrollTop: $("#cert").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+  $("#trainingbtn").click(function () {
+    $(".toggle").css("border", "5px solid #fff");
+    $("html, body").animate(
+      {
+        scrollTop: $("#training").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
 
-	$("#workbtn").click(function () {
-		$('.toggle').css('border', '5px solid #fff')
-		$("html, body").animate({
-			scrollTop: $("#workexp").offset().top
-		}, 1000);
-		return false;
-	});
+  var $win = $(window);
+  var winH = $win.height(); // Get the window height.
 
-	$("#certbtn").click(function () {
-		$('.toggle').css('border', '5px solid #fff')
-		$("html, body").animate({
-			scrollTop: $("#cert").offset().top
-		}, 1000);
-		return false;
-	});
-
-	var $win = $(window);
-	var winH = $win.height();   // Get the window height.
-
-	$win.on("scroll", function () {
-		if ($(this).scrollTop() > winH ) {
-			$('.toggle').css('border', '5px solid #fff')
-		} else {
-			$('.toggle').css('border', '5px solid #000')
-		}
-	}).on("resize", function(){ // If the user resizes the window
-	winH = $(this).height(); // you'll need the new height value
-	});
+  $win
+    .on("scroll", function () {
+      if ($(this).scrollTop() > winH) {
+        $(".toggle").css("border", "5px solid #fff");
+      } else {
+        $(".toggle").css("border", "5px solid #000");
+      }
+    })
+    .on("resize", function () {
+      // If the user resizes the window
+      winH = $(this).height(); // you'll need the new height value
+    });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  let togglebtn = document.querySelector(".toggle"),
+    navcontainer = document.querySelector(".nav-container"),
+    links = document.querySelectorAll(".navigation ul li a"),
+    maincontent = document.querySelector(".main-content"),
+    reveals = document.querySelectorAll(".reveal");
 
+  window.addEventListener("scroll", function () {
+    var winheight = window.innerHeight;
+    reveals.forEach((rev, indx) => {
+      var revealtop = rev[indx].getBoundingClientRect().top;
 
-document.addEventListener('DOMContentLoaded', () => {
-	let
-		togglebtn = document.querySelector('.toggle'),
-		navcontainer = document.querySelector('.nav-container'),
-		links = document.querySelectorAll('.navigation ul li a'),
-		maincontent = document.querySelector('.main-content'),
-		reveals = document.querySelectorAll('.reveal')
+      var revealpoint = 150;
 
+      if (revealtop < winheight - revealpoint) {
+        rev[indx].classList.add("active");
+      } else {
+        rev[indx].classList.remove("active");
+      }
+    });
+  });
+  if (window.innerWidth > 599) {
+    links.forEach((link) => {
+      link.onmouseenter = function () {
+        if (navcontainer.classList.contains("min")) {
+          navcontainer.classList.add("expand");
+        }
+      };
+      link.onmouseleave = function () {
+        if (navcontainer.classList.contains("min")) {
+          navcontainer.classList.remove("expand");
+        }
+      };
+    });
+  } else {
+    links.forEach((link) => {
+      link.onmouseleave = function () {};
+      link.onmouseenter = function () {};
+    });
+  }
+  if (window.innerWidth < 600 && !togglebtn.classList.contains("min")) {
+    maincontent.style.marginLeft = "60px";
+  } else if (window.innerWidth > 599 && !togglebtn.classList.contains("min")) {
+    maincontent.style.marginLeft = "260px";
+  }
 
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 599) {
+      links.forEach((link) => {
+        link.onmouseenter = function () {
+          if (navcontainer.classList.contains("min")) {
+            navcontainer.classList.add("expand");
+          }
+        };
+        link.onmouseleave = function () {
+          if (navcontainer.classList.contains("min")) {
+            navcontainer.classList.remove("expand");
+          }
+        };
+      });
+    } else {
+      links.forEach((link) => {
+        link.onmouseleave = function () {};
+        link.onmouseenter = function () {};
+      });
+    }
 
-	window.addEventListener('scroll', function () {
-		var winheight = window.innerHeight;
-		reveals.forEach((rev, indx) => {
-			
-			var revealtop = rev[indx].getBoundingClientRect().top;
+    if (window.innerWidth < 600 && !togglebtn.classList.contains("min")) {
+      maincontent.style.marginLeft = "60px";
+    } else if (
+      window.innerWidth > 599 &&
+      !togglebtn.classList.contains("min")
+    ) {
+      maincontent.style.marginLeft = "260px";
+    }
+  });
 
-			var revealpoint = 150;
+  togglebtn.addEventListener("click", function () {
+    this.classList.toggle("min");
+    navcontainer.classList.toggle("min");
 
-			if (revealtop < winheight - revealpoint){
-				rev[indx].classList.add('active')
-			}
-			else{
-				rev[indx].classList.remove('active')
-			}	
-		})
-	})
-	if(window.innerWidth > 599){
-		links.forEach(link => {
-			link.onmouseenter = function () {
-				if (navcontainer.classList.contains('min')) {
-					navcontainer.classList.add('expand')
-				}
-			}
-			link.onmouseleave = function () {
+    if (this.classList.contains("min")) {
+      maincontent.style.marginLeft = "60px";
+      togglebtn.setAttribute("data-bs-original-title", "Expand");
+    } else {
+      maincontent.style.marginLeft = "260px";
+      togglebtn.setAttribute("data-bs-original-title", "Minimize");
+    }
+  });
 
-				if (navcontainer.classList.contains('min')) {
-					navcontainer.classList.remove('expand')
-				}
-			}
-		})
-	}else{
-		links.forEach(link => { 
-				link.onmouseleave = function(){} 
-				link.onmouseenter = function () {}
-		})
-	}
-	if (window.innerWidth < 600 && !togglebtn.classList.contains('min')){
-		maincontent.style.marginLeft = "60px";
-	}
-	else if (window.innerWidth > 599 && !togglebtn.classList.contains('min')){
-		maincontent.style.marginLeft = "260px";
-	}
+  const sr = ScrollReveal({
+    origin: "bottom",
+    duration: 1000,
+    easing: "ease-in",
+    delay: 300,
+    reset: true,
+  });
+  sr.reveal(".cm-card", {
+    opacity: 0,
+    origin: "left",
+    distance: "300px",
+    duration: 1500,
+    delay: 200,
+  });
+  sr.reveal(".moon", {
+    opacity: 0,
+    origin: "right",
+    distance: "300px",
+    duration: 1500,
+    delay: 200,
+  });
 
-	window.addEventListener('resize', function(){
-		if(window.innerWidth > 599){
-			links.forEach(link => {
-				link.onmouseenter = function () {
-					if (navcontainer.classList.contains('min')) {
-						navcontainer.classList.add('expand')
-					}
-				}
-				link.onmouseleave = function () {
+  sr.reveal(".edu-info-right", {
+    opacity: 0,
+    origin: "right",
+    distance: "400px",
+    duration: 1000,
+    delay: 100,
+  });
+  sr.reveal(".edu-icon-right", {
+    opacity: 0,
+    origin: "right",
+    distance: "100px",
+    duration: 1000,
+    delay: 100,
+  });
 
-					if (navcontainer.classList.contains('min')) {
-						navcontainer.classList.remove('expand')
-					}
-				}
-			})
-		}else{
-			links.forEach(link => { 
-				link.onmouseleave = function(){} 
-				link.onmouseenter = function () {}
-				}
-			)
-		}
+  sr.reveal(".edu-info-left", {
+    opacity: 0,
+    origin: "left",
+    distance: "400px",
+    duration: 1000,
+    delay: 100,
+  });
+  sr.reveal(".edu-icon-left", {
+    opacity: 0,
+    origin: "left",
+    distance: "100px",
+    duration: 1000,
+    delay: 100,
+  });
 
-		if (window.innerWidth < 600 && !togglebtn.classList.contains('min')){
-			maincontent.style.marginLeft = "60px";
-		}
-			
-		else if (window.innerWidth > 599 && !togglebtn.classList.contains('min')){
-			maincontent.style.marginLeft = "260px";
-		};
-	})
-	
-	
+  sr.reveal(
+    "#skills .sec-title, #education .sec-title, #projects .sec-title, #workexp .sec-title, #cert .sec-title",
+    { origin: "top", distance: "200px", opacity: 0, delay: 300 }
+  );
 
-	togglebtn.addEventListener('click', function () {
-
-		this.classList.toggle('min')
-		navcontainer.classList.toggle('min')
-
-		if (this.classList.contains('min')) {
-			maincontent.style.marginLeft = "60px";
-			togglebtn.setAttribute("data-bs-original-title", "Expand")
-
-		} else {
-			maincontent.style.marginLeft = "260px";
-			togglebtn.setAttribute("data-bs-original-title", "Minimize")
-		}
-
-	})
-
-	
-
-	const sr = ScrollReveal({
-		origin: 'bottom',
-		duration: 1000,
-		easing: 'ease-in',													
-		delay: 300,
-		reset: true
-	});
-	sr.reveal('.cm-card', { opacity: 0,origin: 'left', distance: '300px', duration: 1500, delay: 200});
-	sr.reveal('.moon', { opacity: 0,origin: 'right', distance: '300px', duration: 1500, delay: 200});
-	
-	sr.reveal('.edu-info-right', {opacity: 0,  origin: 'right', distance: '400px', duration: 1000, delay: 100})
-	sr.reveal('.edu-icon-right', {opacity: 0,  origin: 'right', distance: '100px', duration: 1000, delay: 100})
-
-	sr.reveal('.edu-info-left', { opacity: 0, origin: 'left', distance: '400px', duration: 1000, delay: 100})
-	sr.reveal('.edu-icon-left', { opacity: 0, origin: 'left', distance: '100px', duration: 1000, delay: 100})
-
-	sr.reveal('#skills .sec-title, #education .sec-title, #projects .sec-title, #workexp .sec-title, #cert .sec-title', { origin: 'top', distance: '200px',opacity: 0, delay: 300});
-
-	sr.reveal('.award-container', { origin: 'center', duration: 1000, opacity: 0, delay: 100});
-	sr.reveal('.skill', { origin: 'bottom', distance: '50px', duration: 1000, opacity: 0, delay: 50});
-})
+  sr.reveal(".award-container", {
+    origin: "center",
+    duration: 1000,
+    opacity: 0,
+    delay: 100,
+  });
+  sr.reveal(".skill", {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+    opacity: 0,
+    delay: 50,
+  });
+});
